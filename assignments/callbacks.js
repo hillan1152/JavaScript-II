e22d6ec6807762d35db440940b61f91ec6e1ad4e
Anthony Hill <hillan1152@gmail.com>
2019-08-20 17:57:53 -0400

    Working on final 3 array-methods

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -40,22 +40,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
+  return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
+const length = () => (items.length);
+console.log(getLength(items, length));
 
 function last(arr, cb) {
+  return cb(arr[arr.length - 1]);
   // last passes the last item of the array into the callback.
-}
+};
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);// sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+sumNums(2, 3, finalSum => console.log(finalSum));
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);// multiplyNums multiplies two numbers and passes the result to the callback.
 }
+multiplyNums(2, 4, finalProduct => console.log(finalProduct));
 
 function contains(item, list, cb) {
+   inArray => {
+     for (i = 0; i < item.length; i++){
+       if (item[i] === list) {
+         return true
+       } else 
+       return false;
+     }
+     cb(inArray());
+  }
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
