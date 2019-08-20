51e60454c2559cae5682d0b06945fb0fa3ed81d9
Anthony Hill <hillan1152@gmail.com>
2019-08-20 18:51:07 -0400

    need to finish 'Be Creative' section

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -69,18 +69,25 @@ let firstNamesAllCaps = [];
   runners.map(function(person) {
   return firstNamesAllCaps.push(person.first_name.toUpperCase());
 }); 
-
 console.log (firstNamesAllCaps);
 // console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
+  const filterShirts = runners.filter(item => {
+    return item.shirt_size === "L";
+  });
+  runnersLargeSizeShirt.push(filterShirts);
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+let ticketPriceTotal = [];
+  let filterDonations = runners.reduce((total, tickets) => {
+      return total += tickets.donation;
+  }, 0);
+  ticketPriceTotal.push(filterDonations);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
